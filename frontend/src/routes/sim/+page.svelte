91fe06a43bbeb6f3a5f7a423ba25d334847e6999
hotaru <hotaru.py@gmail.com>
2024-04-26 00:35:58 +0530

    some minor changes

--- frontend/src/routes/sim/+page.svelte
+++ frontend/src/routes/sim/+page.svelte
@@ -21,6 +21,8 @@
     address: "",
   };
 
+  let animateHit = true;
+
   onMount(() => {
     fetch("http://localhost:8000/")
       .then((res) => res.json())
@@ -35,77 +37,189 @@
       .then((data) => {
         console.log(data);
         resp = data;
+        animateHit = true;
 
-        
+        setTimeout(() => {
+          animateHit = false;
+        }, 250);
       });
   }
 </script>
-
-<div class="flex flex-col justify-between items-center h-screen px-10">
-  <div>
-    <div class="text-center">
-      <div class="flex justify-center space-x-4 py-5">
-        <div class="mb-4 text-[#ADD8E6] rounded-lg p-4 text-xl square"></div>
-        <div class="mb-4 text-center justify-center bg-red-500 text-white rounded-lg p-4 text-3xl square">
-          <p class="py-2">CPU</p>
-          <button on:click={handleClick} class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-1 rounded text-xl">
-            Get memory
-          </button>
+<div class="relative bg-svg">
+  <div class="flex flex-col justify-between items-center h-screen px-10">
+    <div>
+      <div class="text-center">
+        <div class="flex justify-center space-x-4 py-5">
+          <div class="mb-4 text-[#ADD8E6] rounded-lg p-4 text-xl square"></div>
+          <div class="mb-4 text-center justify-center bg-red-500 text-white rounded-lg p-4 text-3xl cpu">
+            <p class="py-2">CPU</p>
+            <button on:click={handleClick} class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-1 rounded text-xl">
+              Get memory
+            </button>
+          </div>
+          <div class="mb-4 text-[#ADD8E6] rounded-lg p-4 text-xl square"></div>
         </div>
-        <div class="mb-4 text-[#ADD8E6] rounded-lg p-4 text-xl square"></div>
-      </div>
-      <div class="flex justify-center space-x-4 py-5">
-        <div id='l1' class="flex-none bg-green-500 text-white rounded-lg p-4 text-xl box px-5">
-          <p class="text-xl mb-2">L1 Cache</p>
-          <div class="overflow-y-auto h-32">
-            <table class="w-full">
-              {#each resp["L1_cache"] as line}
-                <tr><td style="font-size: 12px;">{line}</td></tr>
-              {/each}
-            </table>
+        <div class="flex justify-center space-x-4 py-5">
+          <div id='l1' class="flex-none bg-green-500 text-white rounded-lg p-4 text-xl box px-5 {resp.L1_cache_hit === 'Hit' && animateHit ? 'l1-hit' : ''}">
+            <p class="text-xl mb-2">L1 Cache</p>
+            <div class="overflow-y-auto h-32">
+              <table class="w-full">
+                {#each resp["L1_cache"] as line}
+                  <tr><td style="font-size: 12px;">{line}</td></tr>
+                {/each}
+              </table>
+            </div>
+          </div>
+          <div id='v' class="flex-none bg-blue-500 text-white rounded-lg p-4 text-xl box px-5 {resp.Victim_cache_hit === 'Hit' && animateHit ? 'v-hit' : ''}">
+            <p class="text-xl mb-2">Victim Cache</p>
+            <div class="overflow-y-auto h-32">
+              <table class="w-full">
+                {#each resp.Victim_cache as line}
+                  <tr><td style="font-size: 12px;">{line}</td></tr>
+                {/each}
+              </table>
+            </div>
+          </div>
+          <div id='l2' class="flex-none bg-yellow-500 text-white rounded-lg p-4 text-xl box px-5 {resp.L2_cache_hit === 'Hit' && animateHit ? 'l2-hit' : ''}">
+            <p class="text-xl mb-2">L2 Cache</p>
+            <div class="overflow-y-auto h-32">
+              <table class="w-full">
+                {#each resp.L2_cache as line}
+                  <tr><td style="font-size: 12px;">{line}</td></tr>
+                {/each}
+              </table>
+            </div>
           </div>
         </div>
-        <div id='v' class="flex-none bg-blue-500 text-white rounded-lg p-4 text-xl box px-5">
-          <p class="text-xl mb-2">Victim Cache</p>
-          <div class="overflow-y-auto h-32">
-            <table class="w-full">
-              {#each resp.Victim_cache as line}
-                <tr><td style="font-size: 12px;">{line}</td></tr>
-              {/each}
-            </table>
-          </div>
+        <div id='ram' class="mt-4 bg-purple-500 text-white rounded-lg p-4 text-4xl py-2 {resp.L1_cache_hit === 'Miss' && resp.Victim_cache_hit === 'Miss' && resp.L2_cache_hit === 'Miss' && animateHit ? 'ram-hit'  : ''}">
+          RAM
         </div>
-        <div id='l2' class="flex-none bg-yellow-500 text-white rounded-lg p-4 text-xl box px-5">
-          <p class="text-xl mb-2">L2 Cache</p>
-          <div class="overflow-y-auto h-32">
-            <table class="w-full">
-              {#each resp.L2_cache as line}
-                <tr><td style="font-size: 12px;">{line}</td></tr>
-              {/each}
-            </table>
-          </div>
-        </div>
-      </div>
-      <div id='ram' class="mt-4 bg-purple-500 text-white rounded-lg p-4 text-4xl py-2">
-        RAM
       </div>
     </div>
-  </div>
-  <div class="flex justify-center mt-2 items-center py-2">
-    <p class="px-5 text-2xl">Address: {resp.address?resp.address:"-"}</p>
-    <p class="px-5 text-2xl">L1 Cache: {resp.L1_cache_hit?resp.L1_cache_hit:"-"}</p>
-    <p class="px-5 text-2xl">Victim Cache: {resp.Victim_cache_hit?resp.Victim_cache_hit:"-"}</p>
-    <p class="px-5 text-2xl">L2 Cache: {resp.L2_cache_hit?resp.L2_cache_hit:"-"}</p>
+    <div class="flex justify-center mt-2 items-center py-2">
+      <p class="px-5 text-2xl">Address: {resp.address?resp.address:"-"}</p>
+      <p class="px-5 text-2xl">L1 Cache: {resp.L1_cache_hit?resp.L1_cache_hit:"-"}</p>
+      <p class="px-5 text-2xl">Victim Cache: {resp.Victim_cache_hit?resp.Victim_cache_hit:"-"}</p>
+      <p class="px-5 text-2xl">L2 Cache: {resp.L2_cache_hit?resp.L2_cache_hit:"-"}</p>
+    </div>
   </div>
 </div>
 
 <style>
+  @keyframes ripple {
+    0% {
+      transform: scale(0);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(0.5);
+      opacity: 0;
+    }
+  }
+  @keyframes l1-1 {
+    0%, 100% {
+      transform: translate(0, 0);
+    }
+    50% {
+      transform: translate(150px, 0);
+    }
+    100% {
+      transform: translate(150px, 150px);
+    }
+  }
+  @keyframes l1-2 {
+    0%, 100% {
+      transform: translate(0, 0);
+    }
+    50% {
+      transform: translate(150px, 0);
+    }
+    100% {
+      transform: translate(150px, 150px);
+    }
+  }
+  @keyframes v-1 {
+    0%, 100% {
+      transform: translate(0, 0);
+    }
+    50% {
+      transform: translate(150px, 0);
+    }
+    100% {
+      transform: translate(150px, 150px);
+    }
+  }
+  @keyframes v-2 {
+    0%, 100% {
+      transform: translate(0, 0);
+    }
+    50% {
+      transform: translate(150px, 0);
+    }
+    100% {
+      transform: translate(150px, 150px);
+    }
+  }
+  @keyframes l2-1 {
+    0%, 100% {
+      transform: translate(0, 0);
+    }
+    50% {
+      transform: translate(150px, 0);
+    }
+    100% {
+      transform: translate(150px, 150px);
+    }
+  }
+  @keyframes l2-2 {
+    0%, 100% {
+      transform: translate(0, 0);
+    }
+    50% {
+      transform: translate(150px, 0);
+    }
+    100% {
+      transform: translate(150px, 150px);
+    }
+  }
+  @keyframes ram-1 {
+    0%, 100% {
+      transform: translate(0, 0);
+    }
+    50% {
+      transform: translate(150px, 0);
+    }
+    100% {
+      transform: translate(150px, 150px);
+    }
+  }
+  .l1-hit {
+    animation: l1-1 0.25s ease, 0.25s ease-out;
+  }
+  .v-hit {
+    animation: v-1 0.25s ease, ripple 0.25s ease-out;
+  }
+  .l2-hit {
+    animation: l2-1 0.25s ease, ripple 0.25s ease-out;
+  }
+  .ram-hit {
+    animation: ram-1 0.25s ease, ripple 0.25s ease-out;
+  }
   .square {
-    width: 500px;
+    width: 400px;
+    height: 150px;
+  }
+  .cpu {
+    width: 700px;
     height: 150px;
   }
   .box {
     width: 500px;
     height: 400px;
+  }
+  .bg-svg {
+  background-image: url('lines.svg');
+  background-size: cover;
+  background-repeat: no-repeat;
   }
 </style>
